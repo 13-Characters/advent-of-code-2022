530f7ba74438c3b6e350a10afac5a68433bdb173
13Characters_ <41973049+13-Characters@users.noreply.github.com>
2022-12-24 22:57:00 -0800

    finish code

--- day25/part1.py
+++ day25/part1.py
@@ -1,6 +1,9 @@
 input = open("input.txt").readlines()
 
 sum = 0
+values = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2}
 for number in input:
-    for place, digit in enumerate(number):
-        if 
+    for place, digit in enumerate(number.replace("\n", "")):
+        sum += values[digit] * (5 ** (len(number) - 1 - place)
+
+print(sum)
