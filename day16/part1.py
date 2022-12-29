c5694c219b64fcd41de28be7c05fc4e174a8bafc
13Characters_ <41973049+13-Characters@users.noreply.github.com>
2022-12-29 14:52:09 -0800

    Part 1 completed

--- day16/part1.py
+++ day16/part1.py
@@ -1,7 +1,9 @@
-# This solution is incomplete.
 import re
 import itertools
-input = open("example_input.txt").readlines()
+from collections import deque
+import time as tim
+input = open("input.txt").readlines()
+start_time = tim.time_ns()
 class Valve():
     def __init__(self, name, flowRate, adjacents):
         self.name = name
@@ -28,7 +30,6 @@ for line in input:
     name = names[0]
     flowRate = int(re.search('\d+', line)[0])
     valves.append(Valve(name, flowRate, []))
-
 for i, line in enumerate(input):
     names = re.findall('[A-Z][A-Z]', line)
     adjacents = []
@@ -38,26 +39,51 @@ for i, line in enumerate(input):
                 adj = v
         adjacents.append(adj)
     valves[i].adjacents = adjacents
-
+end_parse_time = tim.time_ns()
 for v in valves:
     if v.name == 'AA':
         currentValve = v
-stop = False
-openOrder = []
-for v in valves:
-    if v.flowRate > 0:
-        openOrder.append(v)
-openOrder.insert(0, currentValve)
-openOrder.sort(key=lambda x: currentValve.getDistance(x)) # First guess, we will refine this later
-maxPressure = 0
-time = 0
-totalPressure = 0
-for i in range(1, len(openOrder)):
-    valve = openOrder[i]
-    previousValve = openOrder[i - 1]
-    pressureReleased = valve.flowRate * (30 - time - (previousValve.getDistance(valve) + 1))
-    totalPressure += pressureReleased
-    print(f"Minute {time + 1}: Move from {previousValve.name} -> {valve.name}, "
-            f"{pressureReleased} released after opening {valve.name}")
-    time += previousValve.getDistance(valve) + 1
-print(str(totalPressure))
+openableValves = {v for v in valves if v.flowRate > 0}
+
+
+
+def calcPressure(path, startingValve, time=0, pressure=0):
+    # Starting conditions
+    time = time
+    totalPressure = int(pressure)
+    path = [startingValve] + list(path)
+
+    for i in range(1, len(path)):
+        valve = path[i]
+        previousValve = path[i - 1]
+        pressureReleased = valve.flowRate * (30 - time - (previousValve.getDistance(valve) + 1))
+        time += previousValve.getDistance(valve) + 1
+        if time > 30: break
+        totalPressure += pressureReleased
+    return valve, time, totalPressure
+
+def findBestPath(path, currentValve, startingTime=0):
+    maxPressure = 0
+    averagePressure = 0
+    if len(path) < 2: iterate = enumerate(itertools.permutations(path))
+    else: iterate = enumerate(itertools.permutations(path, 2))
+    for count, segment in iterate:
+        startingTime = startingTime
+        valve, time, partialPressure = calcPressure(segment, currentValve, time=startingTime)
+        averagePressure = (averagePressure * count + partialPressure) / (count + 1)
+        if partialPressure > averagePressure:
+            remaining = path - set(segment)
+            if remaining and time < 30:
+                totalPressure = partialPressure + findBestPath(remaining, valve, startingTime=time)
+            else:
+                totalPressure = partialPressure
+            if totalPressure > maxPressure:
+                maxPressure = totalPressure
+        if startingTime == 0 and count % 10 == 0:
+            print(count, f"{(tim.time_ns() - start_time) / 1000000000} s")
+    return int(maxPressure)
+
+print(findBestPath(openableValves, currentValve))
+end_time = tim.time_ns()
+print(f"Parsing time: {(end_parse_time - start_time) / 1000000000} seconds\n"
+      f"Check path time: {(end_time - start_time) / 1000000000} seconds")
